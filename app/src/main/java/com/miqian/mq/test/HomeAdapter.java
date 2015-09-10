06569e6a52adeb78ccc6f44b2c8c7b69c7abe27e
suny <suny@shicaidai.com>
2015-09-10 13:47:29 +0800

    fixed NullPointerException

--- app/src/main/java/com/miqian/mq/test/HomeAdapter.java
+++ app/src/main/java/com/miqian/mq/test/HomeAdapter.java
@@ -68,8 +68,8 @@ public class HomeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
       return ADS;//轮播
     } else if (position == 1) {
       return DEMAND_DEPOSIT;//活期
-    //}else if (position == getBasicItemCount() && hasFooter) {
-    }else if (position == 4) {
+      //}else if (position == getBasicItemCount() && hasFooter) {
+    } else if (position == 4) {
       return TYPE_FOOTER;
     } else {
       return REGULAR_DEPOSIT;//定期
@@ -89,12 +89,15 @@ public class HomeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     switch (viewType) {
       case ADS:
-        ViewGroup vImage = (ViewGroup) mInflater.inflate(R.layout.fragment_home_recyclerview_item_advertisement, parent, false);
+        ViewGroup vImage =
+            (ViewGroup) mInflater.inflate(R.layout.fragment_home_recyclerview_item_advertisement,
+                parent, false);
         ViewHolder0 vhImage = new ViewHolder0(vImage);
         return vhImage;
       case DEMAND_DEPOSIT:
         View view =
-            mInflater.inflate(R.layout.fragment_home_recyclerview_item_demand_deposit, parent, false);
+            mInflater.inflate(R.layout.fragment_home_recyclerview_item_demand_deposit, parent,
+                false);
         return new ViewHolder2(view);
       case TYPE_FOOTER: //// TODO: 9/6/15  should refine this load more function
         //View footer = LayoutInflater.from(parent.getContext())
@@ -102,9 +105,9 @@ public class HomeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         //return new FooterViewHolder(footer);
 
       default:
-        View v =
-            mInflater.inflate(R.layout.fragment_home_recyclerview_item_regular_deposit, parent, false);
-        final CircleBar circleBar = (CircleBar)v.findViewById(R.id.circlebar);
+        View v = mInflater.inflate(R.layout.fragment_home_recyclerview_item_regular_deposit, parent,
+            false);
+        final CircleBar circleBar = (CircleBar) v.findViewById(R.id.circlebar);
         circleBar.setSweepAngle(270);//todo keen control the angle
         //circleBar.setOnClickListener(new View.OnClickListener() {
         //  @Override public void onClick(View view) {
@@ -122,7 +125,7 @@ public class HomeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
   @Override public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int i) {
     //没有更多数据
-    if(hasMoreData){
+    if (hasMoreData) {
       ((FooterViewHolder) viewHolder).mProgressView.setVisibility(View.VISIBLE);
       ((FooterViewHolder) viewHolder).mProgressView.startProgress();
       //((FooterViewHolder) holder).mProgressView.setIndeterminate(true);
@@ -160,35 +163,41 @@ public class HomeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
   private boolean hasFooter;
 
   private boolean hasMoreData;
+
   public boolean hasMoreData() {
     return hasMoreData;
   }
+
   public void setHassMoreData(boolean isMoreData) {
-    if(this.hasMoreData != isMoreData) {
+    if (this.hasMoreData != isMoreData) {
       this.hasMoreData = isMoreData;
       notifyDataSetChanged();
     }
   }
+
   public void setHasMoreDataAndFooter(boolean hasMoreData, boolean hasFooter) {
-    if(this.hasMoreData != hasMoreData || this.hasFooter != hasFooter) {
+    if (this.hasMoreData != hasMoreData || this.hasFooter != hasFooter) {
       this.hasMoreData = hasMoreData;
       this.hasFooter = hasFooter;
       notifyDataSetChanged();
     }
   }
+
   public boolean hasFooter() {
     return hasFooter;
   }
 
   public void setHasFooter(boolean hasFooter) {
-    if(this.hasFooter != hasFooter) {
+    if (this.hasFooter != hasFooter) {
       this.hasFooter = hasFooter;
       notifyDataSetChanged();
     }
   }
+
   private List<String> mValues;//// TODO: 9/6/15 数据源
+
   public int getBasicItemCount() {
-return 1;
+    return 1;
     //return mValues.size();
   }
 
@@ -202,13 +211,11 @@ return 1;
       mTextView = (TextView) view.findViewById(R.id.tv_content);
     }
 
-    @Override
-    public String toString() {
+    @Override public String toString() {
       return super.toString() + " '" + mTextView.getText();
     }
   }
 
-  
   //// TODO: 9/6/15  viewpager indicator \\
   private HomeAdPageAdapter mAdpagerAdapter;// 首页焦点图adapter
   private int currentPageState = ViewPager.SCROLL_STATE_IDLE;// Viewpager状态
@@ -218,10 +225,8 @@ return 1;
   private PageIndicator mHomeViewpagerIndictor;// 首页焦点图滑动标记
   private ImageView mHomeFirstHead;// 首发头条
 
-
   /**
    * 首页焦点图滑动监听
-   *
    */
   class MyOnPageChangeListener implements ViewPager.OnPageChangeListener {
 
@@ -239,17 +244,18 @@ return 1;
     }
   }
 
-
-  public  Handler handler = new Handler() {
+  public Handler handler = new Handler() {
     public void handleMessage(Message msg) {
       switch (msg.what) {
         case MSG_ACTION_SLIDE_PAGE:
           if (currentPageState == ViewPager.SCROLL_STATE_IDLE) {
-
-            if (mHomeViewpager.getAdapter() != null) {
-              int current = mHomeViewpager.getCurrentItem();
-              // 获取当前的索引加一，以滑动到下一项
-              mHomeViewpager.setCurrentItem((current + 1), true);
+            //Fixed by keen: NullPointerException
+            if (null != mHomeViewpager) {
+              if (mHomeViewpager.getAdapter() != null) {
+                int current = mHomeViewpager.getCurrentItem();
+                // 获取当前的索引加一，以滑动到下一项
+                mHomeViewpager.setCurrentItem((current + 1), true);
+              }
             }
           }
           break;
@@ -257,8 +263,6 @@ return 1;
           break;
       }
     }
-
-    ;
   };
 
   public void initPagerIndicator(View view) {
@@ -266,7 +270,9 @@ return 1;
     mHomeViewpagerIndictor.setPageOrginal(true);
     mHomeViewpagerIndictor.setTotalPageSize(listEntity.size());
   }
+
   ArrayList<ActivityEntity> listEntity;
+
   /**
    * keen
    * 初始化adapter
@@ -310,7 +316,6 @@ return 1;
           //do nothing
         }
       });
-
     }
   }
 }
