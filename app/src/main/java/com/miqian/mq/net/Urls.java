afa3fe58eb31415627eced090ead0838bee5b640
lt <tuliangtan@shicaidai.com>
2015-10-08 21:08:31 +0800

    merge code

--- app/src/main/java/com/miqian/mq/net/Urls.java
+++ app/src/main/java/com/miqian/mq/net/Urls.java
@@ -89,7 +89,7 @@ public class Urls {
     //details for regular deposit
     public final static String detailsOfRegularDeposit = base_server + "commonService/getHome";
     //records of capital
-    public final static String recordsCapital = base_server + "commonService/getHome";
+    public final static String recordsCapital = base_server + "userService/getAssetRecord";
 
     //我的促销接口
     public final static String getCustPromotion = base_server + "userService/getCustPromotion";
