610b97970eeafa49711add22c9a77242c6900dc2
jackie <jackie.xp@qq.com>
2015-10-16 18:13:37 +0800

    nameValuePair

--- app/src/main/java/com/miqian/mq/net/Param.java
+++ app/src/main/java/com/miqian/mq/net/Param.java
@@ -4,14 +4,28 @@ package com.miqian.mq.net;
  * Created by Administrator on 2015/9/4.
  */
 public class Param {
-    String key;
-    String value;
-
-    public Param() {
-    }
 
     public Param(String key, String value) {
         this.key = key;
+        this.value = value;
+    }
+
+    String key;
+    String value;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
         this.value = value;
     }
 }
