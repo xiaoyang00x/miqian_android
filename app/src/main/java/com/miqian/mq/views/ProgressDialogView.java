02b078ab0e6e6dc935ab39997f0ed955bd0fc59b
lt <tuliangtan@shicaidai.com>
2016-01-27 17:56:55 +0800

    加载动画

--- app/src/main/java/com/miqian/mq/views/ProgressDialogView.java
+++ app/src/main/java/com/miqian/mq/views/ProgressDialogView.java
@@ -2,12 +2,14 @@ package com.miqian.mq.views;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.miqian.mq.R;
@@ -24,6 +26,11 @@ public class ProgressDialogView {
 
 	public static Dialog create(Context context) {
 		view = LayoutInflater.from(context).inflate(R.layout.progressdialog_layout, null);
+		ImageView iv_animation= (ImageView) view.findViewById(R.id.iv_animation);
+		iv_animation.setImageResource(R.drawable.animation_loading);
+		AnimationDrawable animationDrawable = (AnimationDrawable) iv_animation.getDrawable();
+		animationDrawable.start();
+
 		dialog = new Dialog(context, R.style.loading_dialog);
 		Window dialogWindow = dialog.getWindow();
 		WindowManager.LayoutParams lp = dialogWindow.getAttributes();
