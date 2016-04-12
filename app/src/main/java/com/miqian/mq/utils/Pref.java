4e4594f1d2fc632647aae950a8e850c18318cb18
jackie <jackie.xp@qq.com>
2016-04-12 16:54:33 +0800

    1.连连错误配置表

--- app/src/main/java/com/miqian/mq/utils/Pref.java
+++ app/src/main/java/com/miqian/mq/utils/Pref.java
@@ -29,7 +29,8 @@ public class Pref {
 
     public static final String NICKNAME = "Nickname";
 
-    public static final String USERICON = "Usericon";
+    public static final String ERROR_LIAN = "ErrorLian";
+    public static final String ERROR_LIAN_VERSION = "ErrorLianVersion";
 
     public static final String TELEPHONE = "Telephone";
 
