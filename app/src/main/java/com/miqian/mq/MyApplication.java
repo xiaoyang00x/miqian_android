983b48e66fc22b0e9ad73e3bc95696f181e572da
jackie <jackie.xp@qq.com>
2016-11-15 09:41:38 +0800

    显示我的邀请

--- app/src/main/java/com/miqian/mq/MyApplication.java
+++ app/src/main/java/com/miqian/mq/MyApplication.java
@@ -1,6 +1,5 @@
 package com.miqian.mq;
 
-import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.support.multidex.MultiDexApplication;
