fb82489421f329c04acb3b9ce319af81ba5c2d04
lt <tuliangtan@shicaidai.com>
2016-12-22 18:36:46 +0800

    Revert "提现开关问题。 银行名默认去掉"
    This reverts commit f409a4bb86abe81baed696225f756b629eaa86cf.

--- app/src/main/java/com/miqian/mq/fragment/FragmentUser.java
+++ app/src/main/java/com/miqian/mq/fragment/FragmentUser.java
@@ -274,7 +274,7 @@ public class FragmentUser extends BasicFragment implements View.OnClickListener,
             } else {
                 btn_message.setImageResource(R.drawable.btn_message);
             }
-        } else {
+        }else {
             btn_message.setVisibility(View.GONE);
         }
         btn_message.setOnClickListener(this);
@@ -456,7 +456,7 @@ public class FragmentUser extends BasicFragment implements View.OnClickListener,
                     if (new BigDecimal(balance).compareTo(new BigDecimal(0)) > 0) {
 
                         if ("1".equals(userInfo.getBindCardStatus())) {
-                            if (userInfo.getWithdrawCashSwitch() == 0) {
+                            if (userInfo.getWithdrawCashSwitch() == 1) {
                                 Uihelper.showToast(getActivity(), R.string.qq_project_rollout);
                             } else {
                                 Intent intent = new Intent(getActivity(), RolloutActivity.class);
