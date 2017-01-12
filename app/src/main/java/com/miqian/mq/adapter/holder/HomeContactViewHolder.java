fddff41be68695d334897133387d93a4a090c6d7
wgl <wanggl@shicaidai.com>
2017-01-12 15:49:57 +0800

    修改首页客服电话 为在线客服

--- app/src/main/java/com/miqian/mq/adapter/holder/HomeContactViewHolder.java
+++ app/src/main/java/com/miqian/mq/adapter/holder/HomeContactViewHolder.java
@@ -1,55 +1,70 @@
 package com.miqian.mq.adapter.holder;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.support.v4.app.ActivityCompat;
+import android.content.Context;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.miqian.mq.R;
 import com.miqian.mq.entity.HomePageInfo;
-import com.umeng.analytics.MobclickAgent;
+import com.miqian.mq.utils.MobileDeviceUtil;
+import com.miqian.mq.utils.Pref;
+import com.miqian.mq.utils.UserUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import cn.udesk.UdeskConst;
+import cn.udesk.UdeskSDKManager;
 
 /**
  * Created by guolei_wang on 16/5/24.
  * 首页客服电话
  */
 public class HomeContactViewHolder extends HomeBaseViewHolder {
-    private TextView tv_tel;
+    private TextView tv_online;
+    private Context mContext;
 
     public HomeContactViewHolder(View itemView) {
         super(itemView);
-
-        tv_tel = (TextView) itemView.findViewById(R.id.tv_tel);
+        mContext = itemView.getContext();
+        tv_online = (TextView) itemView.findViewById(R.id.tv_online);
     }
 
     @Override
     public void bindView(final HomePageInfo mData) {
-        tv_tel.setText(mData.getConsumerHotLine());
-        tv_tel.setOnClickListener(new View.OnClickListener() {
+        tv_online.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                MobclickAgent.onEvent(view.getContext(), "1004_4");
-                if (ActivityCompat.checkSelfPermission(view.getContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                    MobclickAgent.onEvent(view.getContext(), "1004_5");
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    Toast.makeText(view.getContext(),"您尚未开启通话权限，请开启后再尝试。", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                MobclickAgent.onEvent(view.getContext(), "1004_6");
-                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + mData.getConsumerHotLine()));
-                view.getContext().startActivity(intent);
+                setUdeskUserInfo();
+                UdeskSDKManager.getInstance().showRobotOrConversation(mContext);
             }
         });
+    }
+
+    /**
+     * 设置 udesk 所需用户信息
+     */
+    private void setUdeskUserInfo() {
+
+        Map<String, String> info = new HashMap<String, String>();
+        String userId = "";
+        if (UserUtil.hasLogin(mContext)) {
+            userId = UserUtil.getUserId(mContext);
+        } else {
+            userId = MobileDeviceUtil.getInstance(mContext).getMobileImei();
+        }
+        if (TextUtils.isEmpty(userId)) {
+            userId = MobileDeviceUtil.getInstance(mContext).getUUID();
+        }
+
+        info.put(UdeskConst.UdeskUserInfo.USER_SDK_TOKEN, userId);
+        //以下注释的字段都是可选的字段， 有邮箱建议填写
+        info.put(UdeskConst.UdeskUserInfo.NICK_NAME, Pref.getString(Pref.REAL_NAME, mContext, ""));
+        info.put(UdeskConst.UdeskUserInfo.CELLPHONE, Pref.getString(Pref.TELEPHONE, mContext, MobileDeviceUtil.getInstance(mContext).getPhoneNum()));
+
+        UdeskSDKManager.getInstance().setUserInfo(mContext, userId, info);
+
     }
 }
